8705d78cf548d2a72601e10c83c9e70b6a8e4eac
GuardianKiller <com_1998@abv.bg>
2024-07-04 23:57:55 +0300

    initial commit

---
+++ src/main/java/bg/guardiankiller/moviessocialapp/MoviesSocialAppApplication.java
@@ -0,0 +1,13 @@
+package bg.guardiankiller.moviessocialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesSocialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesSocialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/guardiankiller/moviessocialapp/MoviesSocialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.guardiankiller.moviessocialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSocialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
