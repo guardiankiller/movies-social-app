9760d18ea2ecfde3123096b445492e9395107c56
guardiankiller <124448649+guardiankiller@users.noreply.github.com>
2024-07-15 18:53:09 +0300

    26 added registration page (#29)
    - Added registration page
    - Added UserController

--- src/main/java/bg/guardiankiller/moviessocialapp/model/entity/User.java
+++ src/main/java/bg/guardiankiller/moviessocialapp/model/entity/User.java
@@ -23,11 +23,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(name = "first_name", nullable = false)
-    private String firstName;
-
-    @Column(name = "last_name", nullable = false)
-    private String lastName;
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
 
     @Column(unique = true, nullable = false)
     private String email;
