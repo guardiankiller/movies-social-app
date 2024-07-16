a544f9aaf0a6c1da3897172ed8bd6804a3efc86a
guardiankiller <124448649+guardiankiller@users.noreply.github.com>
2024-07-16 23:48:44 +0300

    27 Configure spring security part1 (#32)

--- src/main/java/bg/guardiankiller/moviessocialapp/web/RegistrationController.java
+++ src/main/java/bg/guardiankiller/moviessocialapp/web/RegistrationController.java
@@ -2,10 +2,12 @@ package bg.guardiankiller.moviessocialapp.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/users")
-public class UserController {
+public class RegistrationController {
+  @GetMapping("/register")
+  public String getRegister() {
+    return "/register";
+  }
 
 }
