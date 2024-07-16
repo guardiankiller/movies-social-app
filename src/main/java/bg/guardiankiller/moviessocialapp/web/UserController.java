3ac45620f3a4c03a5b358f0b90a8045dbc2d83a3
GuardianKiller <com_1998@abv.bg>
2024-07-16 23:33:47 +0300

    Added UserController

--- src/main/java/bg/guardiankiller/moviessocialapp/web/UserController.java
+++ src/main/java/bg/guardiankiller/moviessocialapp/web/UserController.java
@@ -2,15 +2,10 @@ package bg.guardiankiller.moviessocialapp.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    @GetMapping("/register")
-    public String getRegister() {
-        return "/register";
-    }
-    @GetMapping("/login")
-    public String getLogin() {
-        return "/login";
-    }
+
 }
