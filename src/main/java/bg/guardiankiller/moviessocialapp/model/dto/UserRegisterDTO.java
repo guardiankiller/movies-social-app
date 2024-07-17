4c68e74ed5df34668f098f88b6888f142a329dc4
GuardianKiller <com_1998@abv.bg>
2024-07-17 23:22:04 +0300

    Added UserLoginDTO and UserRegisterDTO

---
+++ src/main/java/bg/guardiankiller/moviessocialapp/model/dto/UserRegisterDTO.java
@@ -0,0 +1,57 @@
+package bg.guardiankiller.moviessocialapp.model.dto;
+
+public class UserRegisterDTO {
+
+    private String username;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private String confirmPassword;
+
+    public UserRegisterDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
