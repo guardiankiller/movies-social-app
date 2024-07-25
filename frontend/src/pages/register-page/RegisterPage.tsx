2589ecef01543e00eb2bb9ccf2f5753354ff7b30
guardiankiller <com_1998@abv.bg>
2024-07-25 18:30:27 +0300

    Added basic form handling

--- frontend/src/pages/register-page/RegisterPage.tsx
+++ frontend/src/pages/register-page/RegisterPage.tsx
@@ -1,8 +1,32 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo-just-watch-together.png'
 import classes from './RegisterPage.module.css'
+import { useEffect } from 'react'
+import createFormReducer from '../../utils/form-reducer'
+import { type RegisterForm, type ViolationResponse } from '../../utils/models'
 
 function RegisterPage() {
+  const initalState: RegisterForm = {
+    username: '', password: '', confirmPassword: '', email: '', fullName: ''
+  }
+
+  const [form, dispatch] = createFormReducer(initalState)
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const response = await fetch('/api/users',  {
+        method: 'POST', // Specify the request method
+        headers: {
+          'Content-Type': 'application/json', // Set the content type to JSON
+        },
+        body: JSON.stringify(form)});
+      if(!response.ok) {
+        const errors: ViolationResponse = await response.json()
+        console.log(errors)
+      }
+    }, 500)
+    return () => clearTimeout(timer)
+  }, [form])
 
   return (
     <div>
@@ -19,32 +43,61 @@ function RegisterPage() {
                 <h1 className={`${classes.h1Margin} ${classes.width}`}>Create account</h1>
                 <div className={`${classes.aSpacingBase}`}>
                   <label htmlFor="username" className={`${classes.regLabel}`}>Username</label>
-                  <input type="text" id="username" name="username" className={`${classes.aInputText} ${classes.aSpan12}`}
+                  <input 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                    className={`${classes.aInputText} ${classes.aSpan12}`}
+                    value={form.username}
+                    onChange={e => dispatch('username', e.target.value)}
                     aria-required="true" />
                 </div>
                 <div className={`${classes.aSpacingBase}`}>
                   <label htmlFor="username" className={`${classes.regLabel}`}>Your name</label>
-                  <input type="text" id="fullName" name="fullName"
+                  <input 
+                    type="text" 
+                    id="fullName" 
+                    name="fullName"
                     className={`${classes.aInputText} ${classes.aSpan12}`}
                     placeholder="First and last name"
+                    value={form.fullName}
+                    onChange={e => dispatch('fullName', e.target.value)}
                     aria-required="true" />
                 </div>
                 <div className={`${classes.aSpacingBase}`}>
                   <label htmlFor="username" className={`${classes.regLabel}`}>Email</label>
-                  <input type="email" id="email" name="email"
+                  <input 
+                    type="email" 
+                    id="email" 
+                    name="email"
                     className={`${classes.aInputText} ${classes.aSpan12}`}
                     placeholder="person@example.com"
+                    value={form.email}
+                    onChange={e => dispatch('email', e.target.value)}
                     aria-required="true" />
                 </div>
                 <div className={`${classes.aSpacingBase}`}>
                   <label htmlFor="password" className={`${classes.regLabel}`}>Password</label>
-                  <input type="password" id="password" placeholder="at least 8 characters" name="password"
-                    className={`${classes.aInputText} ${classes.aSpan12}`} aria-required="true" />
+                  <input 
+                    type="password" 
+                    id="password" 
+                    placeholder="at least 8 characters" 
+                    name="password"
+                    className={`${classes.aInputText} ${classes.aSpan12}`} 
+                    value={form.password}
+                    onChange={e => dispatch('password', e.target.value)}
+                    aria-required="true" />
                 </div>
                 <div className={`${classes.aSpacingBase}`}>
                   <label htmlFor="confirmPassword" className={`${classes.regLabel}`}>Re-enter password</label>
-                  <input type="password" id="confirmPassword" name="confirmPassword"
-                    className={`${classes.aInputText} ${classes.aSpan12}`} aria-required="true" />
+                  <input 
+                    type="password" 
+                    id="confirmPassword" 
+                    name="confirmPassword"
+                    value={form.confirmPassword}
+                    onChange={e => dispatch('confirmPassword', e.target.value)}
+                    className={`${classes.aInputText} ${classes.aSpan12}`} 
+                    aria-required="true" />
                 </div>
                 <div className={`${classes.aSpacingExtraLarge}`}>
                   <span className={`${classes.aButton} ${classes.aButtonSpan12} ${classes.aButtonPrimary}`}>
@@ -53,7 +106,7 @@ function RegisterPage() {
                 </div>
 
                 <div className={`${classes.aDividerInner}`}></div>
-                <div className={`${classes.aRow}`}>Already have an account? 
+                <div className={`${classes.aRow}`}>Already have an account?
                   <Link to="/login" className={`${classes.aLinkEmphasis}`}> Sign in</Link>
                 </div>
               </div>
---
+++ frontend/src/utils/form-reducer.ts
@@ -0,0 +1,23 @@
+import React, { useReducer } from "react"
+
+export class Action<T> {
+    constructor(readonly type: keyof T, readonly input: string) { }
+}
+
+
+function registerReducerFunc<T>(state: T, action: Action<T>): T {
+    const newObj: T = { ...state }
+    newObj[action.type] = action.input as any
+    return newObj
+}
+
+type DipatchFunc<T> = (type: keyof T, input: string) => void
+
+export default function createFormReducer<T>(initalState: T): [T, DipatchFunc<T>] {
+    const [form, dispatch] = useReducer(registerReducerFunc<T>, initalState)
+    
+    function change (type: keyof T, input: string) {
+        dispatch({type, input})
+    }
+    return  [form, change]
+}
